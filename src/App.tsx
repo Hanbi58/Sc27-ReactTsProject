810a49491a79864b10eaa4d4c63b93c23e8c0317
Hanbi58 <hanxiaonan2012@gmail.com>
2022-10-25 11:08:36 -0700

    Form and input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
+import NewTodo from "./components/NewTodo";
 function App() {
   const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
   return (
     <>
+      <NewTodo />
       <Todos items={todos} />
     </>
   );
